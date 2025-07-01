a2c815c4e6c780aed28323fa8272725d5df07124
Reutp111 <reutdevops@gmail.com>
2025-07-01 18:47:53 +0300

    fix the product

---
+++ models/seedProducts.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+const Product = require('./models/product'); // ודאי שהנתיב נכון
+
+mongoose.connect('mongodb://localhost:27017/celebration', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const products = [
+  {
+    name: "#001 - בלונים",
+    description: "ערכה של 10 בלונים מגוונים",
+    category: "עיצוב יום הולדת",
+    price: 30,
+    image: "/images/balloons.jpg"
+  },
+  {
+    name: "#002 - מפה חד פעמית",
+    description: "מפה חד פעמית עם דגלי העולם",
+    category: "כלי שולחן",
+    price: 30,
+    image: "/images/map.jpg"
+  },
+  {
+    name: "#003 - כוסות חד פעמיות",
+    description: "ערכה של 20 כוסות חד פעמיות צבעוניות",
+    category: "כלי שולחן",
+    price: 25,
+    image: "/images/cups.jpg"
+  },
+  {
+    name: "#004 - צלחות חד פעמיות",
+    description: "ערכה של 20 צלחות חד פעמיות מעוצבות",
+    category: "כלי שולחן",
+    price: 25,
+    image: "/images/plates.jpg"
+  },
+  {
+    name: "#005 - קונפטי",
+    description: "ערכת קונפטי ליצירת אווירה קסומה באירוע",
+    category: "אביזרי מסיבה",
+    price: 15,
+    image: "/images/confetti.jpg"
+  },
+  {
+    name: "#006 - כובעי מסיבה",
+    description: "ערכה של 10 כובעי מסיבה עם פסים צבעוניים",
+    category: "אביזרי מסיבה",
+    price: 10,
+    image: "/images/party_hats.jpg"
+  },
+  {
+    name: "#007 - דגלים",
+    description: "ערכה של 10 דגלים לתלייה בפינה או מעל השולחן",
+    category: "אביזרי מסיבה",
+    price: 20,
+    image: "/images/flags.jpg"
+  },
+  {
+    name: "#008 - פיניאטה",
+    description: "פיניאטה בצורת מגלה עם פריטים קטנים בתוכה",
+    category: "אביזרי מסיבה",
+    price: 40,
+    image: "/images/pinata.jpg"
+  },
+  {
+    name: "#009 - אותיות מוארות",
+    description: "אותיות מוארות ליצירת כיתובים מרהיבים",
+    category: "אביזרי מסיבה",
+    price: 30,
+    image: "/images/lighted_letters.jpg"
+  },
+  {
+    name: "#010 - גליל חוט לבלונים",
+    description: "חוט ורוד לבלונים",
+    category: "בלונים",
+    price: 30,
+    image: "/images/HOT.jpg"
+  }
+];
+
+async function seed() {
+  await Product.deleteMany({});
+  await Product.insertMany(products);
+  console.log("כל המוצרים הוזנו בהצלחה!");
+  mongoose.disconnect();
+}
+
+seed();
